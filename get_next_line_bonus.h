a066b7dc8ca68da869c6f8326248f8bd431ae53f
Jaime Nevado Farfán <djamperiio@gmail.com>
2022-11-04 11:54:00 +0100

    bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: jaimenevado <jaimenevado@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 17:41:53 by jaimenevado       #+#    #+#             */
-/*   Updated: 2022/11/02 17:42:02 by jaimenevado      ###   ########.fr       */
+/*   Updated: 2022/11/04 11:34:19 by jaimenevado      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,10 @@
 #  define BUFFER_SIZE 42
 # endif
 
-// GET_NEXT_LINE
 char	*get_next_line(int fd);
 char	*ft_read_fd(int fd, char *save);
 char	*ft_extract_line(char *save);
 char	*ft_new_save(char *save);
-
-// UTILS
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *save, char *buff);
